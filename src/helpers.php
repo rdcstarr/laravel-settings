42bed31a8237627dc178b24f81be903ffd28c716
Rdcstarr <rdcstarr@gmail.com>
2025-09-26 09:00:20 +0300

    7137

--- src/helpers.php
+++ src/helpers.php
@@ -21,3 +21,25 @@ if (!function_exists('settings'))
 		return $setting->get($key, $default);
 	}
 }
+
+if (!function_exists('_s'))
+{
+	/**
+	 * Get or set app settings stored in db.
+	 *
+	 * @param string|null $key
+	 * @param mixed $default
+	 * @return mixed
+	 */
+	function _s(?string $key = null, mixed $default = '')
+	{
+		$setting = app('settings');
+
+		if ($key === null)
+		{
+			return $setting;
+		}
+
+		return $setting->get($key, $default);
+	}
+}
